2e52b63b795e2f4dc5102142de3bcb20ffe82f33
Angelino Capretti <angelino.capretti@me.com>
2017-11-06 15:11:49 +0100

    pre-seed

--- WEEK_6/week6_day1/GoVoyage/db/seeds.rb
+++ WEEK_6/week6_day1/GoVoyage/db/seeds.rb
@@ -3,5 +3,22 @@
 #
 # Examples:
 #
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |n|
+  Flight.create(flight_code: "N°#{n}", duration: "#{n}", datetime: "", departure_airport: "", arrival_airport: "", destination_id: "", origin_id: "")
+end
+
+Airport.create(airport_code: "CDG")
+Airport.create(airport_code: "LUX")
+Airport.create(airport_code: "UKL")
+Airport.create(airport_code: "JFK")
+Airport.create(airport_code: "AMS")
+Airport.create(airport_code: "BXM")
+Airport.create(airport_code: "SPB")
+Airport.create(airport_code: "MLI")
+Airport.create(airport_code: "STU")
+Airport.create(airport_code: "ORL")
+
