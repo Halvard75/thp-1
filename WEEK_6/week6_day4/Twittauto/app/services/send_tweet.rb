53dff8278d7122e1499d5e220483f1bc27baf9f0
Angelino Capretti <angelino.capretti@me.com>
2017-11-09 18:01:39 +0100

    twitter with figaro

--- WEEK_6/week6_day4/Twittauto/app/services/send_tweet.rb
+++ WEEK_6/week6_day4/Twittauto/app/services/send_tweet.rb
@@ -1,24 +1,26 @@
 require 'twitter'
 
 class SendTweet
-#init le string en parametre
+  #init le string en parametre
   def initialize(msg = "tweet 280 babe")
     @msg = msg
     log_in_to_twitter
     send_tweet
   end
 
-#je me log sur twitter
+  #je me log sur twitter
   def log_in_to_twitter
     @client = Twitter::REST::Client.new do |config|
-    	#https://apps.twitter.com/app/14450234
-
+      #https://apps.twitter.com/app/14450234
+      config.consumer_key        = Figaro.env.pusher_key
+      config.consumer_secret     = Figaro.env.pusher_secret
+      config.access_token        = Figaro.env.stripe_api_key
+      config.access_token_secret = Figaro.env.stripe_publishable_key
     end
   end
 
-#j'envoi un tweet
+  #j'envoi un tweet
   def send_tweet
     @client.update(@msg)
   end
-
 end
