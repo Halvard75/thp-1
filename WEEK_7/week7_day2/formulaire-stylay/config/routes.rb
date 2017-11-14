6866c354fbcee841c7a3e6ec608163f217148b56
Angelino Capretti <angelino.capretti@me.com>
2017-11-14 14:36:23 +0100

    stilay

--- WEEK_7/week7_day2/formulaire-stylay/config/routes.rb
+++ WEEK_7/week7_day2/formulaire-stylay/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :videos
   root to: "home#index"
 
-  resources :users do
-  	resources :videos
-  end
+  resources :users
 
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
